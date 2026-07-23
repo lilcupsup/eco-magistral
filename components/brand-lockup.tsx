dac3034602bb075b0571e3b88d0e9a6c1a3486f3
sam <55027255+lilcupsup@users.noreply.github.com>
2026-07-23 16:57:26 +0300

    Add ECO MAGISTRAL brand mark

--- components/brand-lockup.tsx
+++ components/brand-lockup.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import { cn } from "@/lib/utils";
 
 export function BrandLockup({
@@ -15,10 +17,14 @@ export function BrandLockup({
       )}
       aria-label="ECO MAGISTRAL"
     >
-      <span className="brand-mark" aria-hidden="true">
-        <span />
-        <span />
-      </span>
+      <Image
+        src="/images/brand/eco-magistral-mark.png"
+        alt=""
+        aria-hidden="true"
+        width={32}
+        height={32}
+        className="size-8 object-contain"
+      />
       <span className={cn(compact && "hidden sm:inline")}>ECO MAGISTRAL</span>
     </span>
   );
